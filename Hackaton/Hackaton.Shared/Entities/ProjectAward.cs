0cd4c5236ad949b48bf5fbc593a4dcc2714d44d5
sofiaAmaya <laura18amaya@gmail.com>
2024-10-04 09:00:13 -0500

    ~descarga de entityframework ~error en el dbcontext

--- Hackaton/Hackaton.Shared/Entities/ProjectAward.cs
+++ Hackaton/Hackaton.Shared/Entities/ProjectAward.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace Hackaton.Shared.Entities
     public class ProjectAward
 
     {
+        [Display(Name = ("Codigo de la puntuación"))]
+        [MaxLength(4, ErrorMessage = "El {0} puede tener solo 4 caracteres")]
+        [Required(ErrorMessage = "Es obligatorio")]
         public int Id { get; set; }
         public string description { get; set; }
     }
